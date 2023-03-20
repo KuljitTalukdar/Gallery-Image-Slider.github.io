17050f9185a44c7d9b79d1513f370383cab5f5b5
Kuljit <kuljittalukder@gmail.com>
2023-03-20 11:37:06 +0530

    my Gallery-Image-Slider

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SWIPER JS GALLERY ===============*/
+
+let swiperCards = new Swiper(".gallery-cards", {
+    loop: true,
+    loopedSlides: 5,
+    cssMode: true,
+    effect: 'fade',
+});
+
+  let swiperThumbs = new Swiper(".gallery-thumbs", {
+    loop: true,
+    loopedSlides: 5,
+    sliderPerview: 3,
+    centeredSlides: true,
+    sliderToClickedSlide: true,
+
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+swiperThumbs.controller.control = swiperCards
